125dd936a3f629500eee0a728a4f9831f6031f1a
mostafa <immostafaeh@gmail.com>
2024-10-25 16:45:39 +0330

    Personalized right click

---
+++ js/app.js
@@ -0,0 +1,19 @@
+﻿let bod = document.body
+let contextMenu = document.getElementById("contextMenu")
+let logx , logy
+
+
+bod.addEventListener("contextmenu", function(event){
+    event.preventDefault()
+   logx = event.pageX
+   logy = event.pageY
+contextMenu.removeAttribute("style")
+contextMenu.style.left = logx
+contextMenu.style.top = logy
+
+})
+
+bod.addEventListener("click", function(){
+    contextMenu.style.display = "none"
+
+})
